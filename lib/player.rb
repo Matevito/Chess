7c2def2ea33c9e11963fed647e3402d7f43b4bfc
Mateo <madiazt@unal.edu.co>
2020-10-27 16:09:18 -0500

    make move_method(return an array with the data to perform a move)

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,16 @@
 class Player
-    attr_reader :color
+    attr_reader :color, :name
     def initialize(name, color)
         @name = name
         @color = color
     end
     def make_move()
-        #takes a move and returns it corresponding it's color value
+        # takes a move and returns it corresponding it's color value
         puts"\n#{@name}, insert a move:"
         current_move = gets.chomp
+        move = current_move.split("-")
+        #move format [current_piece, where_to_move]
+        # todo: color format of move[0]
+        return move
     end
 end
