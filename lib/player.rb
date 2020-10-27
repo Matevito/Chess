851e5aa8c53693cd09e224e1b091c466956b3c85
Mateo <madiazt@unal.edu.co>
2020-10-27 16:59:40 -0500

    added make_move method castle excpetion

--- lib/player.rb
+++ lib/player.rb
@@ -5,11 +5,16 @@ class Player
         @color = color
     end
     def make_move()
-        # takes a move and returns it corresponding it's color value
         puts"\n#{@name}, insert a move:"
         current_move = gets.chomp
+        # castle exception
+        if current_move == "o-o" || current_move == "o-o-o"
+            return current_move
+        elsif current_move == "O-O" || current_move == "O-O-O"
+            return current_move
+        end
+        # move format [current_piece, where_to_move]
         move = current_move.split("-")
-        #move format [current_piece, where_to_move]
         # todo: color format of move[0]
         return move
     end
