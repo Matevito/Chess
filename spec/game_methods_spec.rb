d164c66cba60cd00bbbd5030c86bdbec71516db4
Mateo <madiazt@unal.edu.co>
2020-10-28 17:36:28 -0500

    corrected chess_to num method

--- spec/game_methods_spec.rb
+++ spec/game_methods_spec.rb
@@ -7,6 +7,6 @@ describe GameMethods
         let(:dummy_class) { Class.new { extend GameMethods } }
         it "transforms a cell into number array" do
             cell = "a3"
-            expect(dummy_class.chess_to_num(cell)).to eql([3,0])
+            expect(dummy_class.chess_to_num(cell)).to eql([2,0])
         end
     end
