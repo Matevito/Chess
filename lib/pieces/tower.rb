a074ad7f429dace66b838bcb019a7cec40bd7578
Mateo <madiazt@unal.edu.co>
2020-10-28 14:32:46 -0500

    added get column method

--- lib/pieces/tower.rb
+++ lib/pieces/tower.rb
@@ -1,19 +1,27 @@
-Class Tower
+require_relative "./board"
+Class Tower < Board
     def possible_moves(player, position, board)
         # the player and board arguments are objects
         color =  player.color
         possible_moves = []
         current_board = board.board
+        current_position = position
         # north column
+        
+    end
+    def get_column(direction, position, color, board)
+        # direction is an array in the for of [x,y]
+        possible_moves = []
         current_position = position
         until current_position[1] > 7
-            current_position[1] += 1
+            current_position[0] += direction[0]
+            current_position[1] += direction[1]
             break unless move_in_board?(current_position)
             row = current_position[0]; column = current_position[1]
-            if current_board[row][column] == " "
+            if board[row][column] == " "
                 possible_moves << current_position
             else
-                board_cell = current_board[row][column]
+                board_cell = board[row][column]
                 if color == "white"
                     if board_cell == board_cell.downcase
                         possible_moves << current_position
@@ -27,7 +35,6 @@ Class Tower
                 end
             end
         end
-
-
+        return possible_moves
     end
 end
