b196f15ec13f4396203cb24db7aac19f681b7805
Mateo <madiazt@unal.edu.co>
2020-10-28 11:01:36 -0500

    added move_in_board? method

--- lib/board.rb
+++ lib/board.rb
@@ -10,4 +10,9 @@ class Board
                     ["p","p","p","p","p","p","p","p"],
                     ["t","n","b","q","k","b","n","t"]]
     end
+    def move_in_board?(position)
+        return false if position[0] < 0 || position[1] < 0
+        return false if position[0] > 7 || position[1] > 7
+        return true
+    end
 end
