a580a9ded3e215f5a6e52e48de14e395125fabef
Mateo <madiazt@unal.edu.co>
2020-10-28 15:42:59 -0500

    added first tower_moves test

--- lib/pieces/tower.rb
+++ lib/pieces/tower.rb
@@ -1,5 +1,5 @@
-require_relative "./board"
-Class Tower < Board
+require_relative "../board"
+class Tower < Board
     def possible_moves(player, position, board)
         # the player and board arguments are objects
         color =  player.color
@@ -8,17 +8,17 @@ Class Tower < Board
         current_position = position
 
         # north, south, east and west columns
-        possible_moves << self.get_column([0,1], current_position, color, board)
-        possible_moves << self.get_column([0,-1], current_position, color, board)
-        possible_moves << self.get_column([1,0], current_position, color, board)
-        possible_moves << self.get_column([-1,0], current_position, color, board)
-
+        possible_moves.concat(self.get_column([0,1], current_position, color, board))
+        possible_moves.concat(self.get_column([0,-1], current_position, color, board))
+        possible_moves.concat(self.get_column([1,0], current_position, color, board))
+        possible_moves.concat(self.get_column([-1,0], current_position, color, board))
         return possible_moves
     end
-    def get_column(direction, position, color, board)
+    def get_column(direction, position, color, board_object)
         # direction is an array in the for of [x,y]
         possible_moves = []
         current_position = position
+        board = board_object.board
         until current_position[1] > 7
             current_position[0] += direction[0]
             current_position[1] += direction[1]
---
+++ spec/tower_spec.rb
@@ -0,0 +1,35 @@
+require "./lib/pieces/tower"
+require "./lib/player"
+require "./lib/board"
+
+describe Tower do
+    describe "#possible moves" do
+        tower = Tower.new
+        white_player = Player.new("1","white")
+        black_player = Player.new("2", "black")
+        it "returns all the valid moves in a clean board" do
+            board = Board.new
+            board.board = [  [" "," "," "," "," "," "," "," "],
+                            [" "," "," "," "," "," "," "," "],
+                            [" "," "," "," "," "," "," "," "],
+                            [" "," "," "," "," "," "," "," "],
+                            [" "," "," "," "," "," "," "," "],
+                            [" "," "," "," "," "," "," "," "],
+                            [" "," "," "," "," "," "," "," "],
+                            [" "," "," "," "," "," "," "," "]]
+            c_cell = [4,4]
+            expect(tower.possible_moves(white_player, c_cell, board)).to eql([[4,5],[4,6],[4,7],[4,3],[4,2],[4,1],[4,0],[5,4],[6,4],[7,4],[3,4],[2,4],[1,4],[0,4]])
+        end
+        xit "dont have possible moves in the beggining(white)" do
+            
+        end
+        xit "dont have possible moves in the beggining(black)" do
+        end
+        xit "handles a board with pieces(white)" do
+            
+        end
+        xit "handles a board with pieces(black)" do
+            
+        end
+    end
+end
