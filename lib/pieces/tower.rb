6200d077971816811af7770e1bc8343aea73ba26
Mateo <madiazt@unal.edu.co>
2020-10-28 16:09:06 -0500

    found bugg in array of possible moves

--- lib/pieces/tower.rb
+++ lib/pieces/tower.rb
@@ -1,4 +1,5 @@
 require_relative "../board"
+require_relative "../player"
 class Tower < Board
     def possible_moves(player, position, board)
         # the player and board arguments are objects
@@ -17,30 +18,43 @@ class Tower < Board
     def get_column(direction, position, color, board_object)
         # direction is an array in the for of [x,y]
         possible_moves = []
-        current_position = position
+        current_position = position.dup.map(&:dup)
         board = board_object.board
-        until current_position[1] > 7
+        until current_position[1] > 7 || current_position[1] < 0 || current_position[0] > 7 || current_position[0] < 0
             current_position[0] += direction[0]
             current_position[1] += direction[1]
             break unless move_in_board?(current_position)
             row = current_position[0]; column = current_position[1]
             if board[row][column] == " "
-                possible_moves << current_position
+                possible_moves.concat(current_position)
             else
                 board_cell = board[row][column]
                 if color == "white"
                     if board_cell == board_cell.downcase
-                        possible_moves << current_position
+                        possible_moves.concat(current_position)
                         break
                     end
                 elsif color == "black"
                     if board_cell == board_cell.upcase
-                        possible_moves << current_position
+                        possible_moves.concat(current_position)
                         break
                     end
                 end
             end
         end
+        p"possible moves for the moment #{possible_moves}"
         return possible_moves
     end
 end
+board = Board.new
+white_player = Player.new("1","white")
+board.board = [  [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," ","T"," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "]]
+tower = Tower.new
+p tower.possible_moves(white_player, [4,4], board)
