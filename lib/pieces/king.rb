254bc7ee0785af6a684558d9817d4ec372703e9b
Mateo <madiazt@unal.edu.co>
2020-10-29 17:45:55 -0500

    added beta of check? method

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -12,7 +12,25 @@ class King < Board
     def possible_moves(player, position, board)
 
     end
-    def check?(color, position, board)
+    def check?(player, position, board)
+        color = player.color
+        current_board = board.board
 
+        if color == "white"
+            for y in range(0..7) do
+                for x in range(0..7)do
+                    c_cell = current_board[y][x]
+                    return true if check_path(c_cell, current_board,"black").include?(position)
+                end
+            end
+        elsif color == "black"
+            for y in range(0..7) do
+                for x in range(0..7)do
+                    c_cell = current_board[y][x]
+                    return true if check_path(c_cell, current_board, "white").include?(position)
+                end
+            end
+        end
+        return false
     end
 end
