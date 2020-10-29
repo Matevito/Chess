89d24d9baaca54e42e3472a411a216f91d0f1183
Mateo <madiazt@unal.edu.co>
2020-10-29 14:18:38 -0500

    added pawn class methods skeleton

---
+++ lib/pieces/pawn.rb
@@ -0,0 +1,18 @@
+require_relative "../board"
+require_relative "../player"
+require_relative "../game_methods"
+
+class Pawn < board
+    include game_methods
+    def possible_moves()
+
+    end
+    def capture_range()
+
+    end
+    def en_passant()
+    
+    end
+    def promotion
+
+    end
