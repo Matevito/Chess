d1b2343f569c2c9101d5405d0c535b59f3ec93f8
Mateo <madiazt@unal.edu.co>
2020-10-28 22:38:44 -0500

    added second and third knight tests

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -20,5 +20,31 @@ describe Knight do
             c_cell = [7,0]
             expect(knight.possible_moves(white_player, c_cell, board)).to eql([[5,1], [6,2]])
         end
+        it "returns all valid moves in the center of a clean board" do
+            board = Board.new
+            board.board = [     [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "],
+                                [" "," "," ","k"," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "]]
+            c_cell = [3,3]
+            expect(knight.possible_moves(black_player, c_cell, board)).to eql([[1,4],[2,5],[4,5],[5,4],
+                [5,2],[4,1],[2,1],[1,2]])
+            
+        end
+        it "returns the valid move in the beggining possition" do
+            board = Board.new
+            c_cell = [7,1]
+            expect(knight.possible_moves(black_player, c_cell, board)).to eql([[5,2],[5,0]])
+        end
+        xit "handles boards with multiple pieces(white)" do
+            board = Board.new
+        end
+        xit "handles boards with multiple pieces(black)" do
+            board = Board.new    
+        end
     end
 end
