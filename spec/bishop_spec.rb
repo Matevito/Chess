29ef2b8046e944e0d38abe20f3621e9906580cf3
Mateo <madiazt@unal.edu.co>
2020-10-28 20:53:02 -0500

    added bishop tests

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -5,32 +5,43 @@ require "./lib/board"
 describe Bishop do
     bishop = Bishop.new
     describe "#possible moves" do
-    white_player = Player.new("1","white")
-    black_player = Player.new("2", "black")
-    it "returns all the valid moves in a clean board" do
-        board = Board.new
+        white_player = Player.new("1","white")
+        black_player = Player.new("2", "black")
+        it "returns all the valid moves in a clean board" do
+            board = Board.new
+                board.board = [  [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," ","B"," "," "," "],
+                                [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "],
+                                [" "," "," "," "," "," "," "," "]]
+                c_cell = [4,4]
+                expect(bishop.possible_moves(white_player, c_cell, board)).to eql([[3,5],[2,6],[1,7],[5,5],[6,6],[7,7],[5,3],[6,2],[7,1],[3,3],[2,2],[1,1],[0,0]])
+        end
+        it "don't have possible moves in the beggining(white)" do
+            board = Board.new
+            c_cell = [0,0]
+            expect(bishop.possible_moves(white_player, c_cell, board)).to eql([])
+        end
+        it "don't have possible moves in the beggining(black)" do
+            board = Board.new
+            c_cell = [7,7]
+            expect(bishop.possible_moves(black_player, c_cell, board)).to eql([])
+        end
+        it "handles a board with pieces" do
+            board = Board.new
             board.board = [  [" "," "," "," "," "," "," "," "],
+                            [" "," "," ","p"," "," "," "," "],
+                            [" ","q"," "," "," "," "," "," "],
+                            [" "," "," "," ","K"," "," "," "],
+                            [" "," "," ","B",""," "," "," "],
                             [" "," "," "," "," "," "," "," "],
-                            [" "," "," "," "," "," "," "," "],
-                            [" "," "," "," "," "," "," "," "],
-                            [" "," "," "," ","B"," "," "," "],
-                            [" "," "," "," "," "," "," "," "],
-                            [" "," "," "," "," "," "," "," "],
-                            [" "," "," "," "," "," "," "," "]]
-            c_cell = [4,4]
-            expect(bishop.possible_moves(white_player, c_cell, board)).to eql([[3,5],[2,6],[1,7],[5,5],[6,6],[7,7],[5,3],[6,2],[7,1],[3,3],[2,2],[1,1],[0,0]])
-    end
-    xit "don't have possible moves in the beggining(white)" do
-        
-    end
-    xit "don't have possible moves in the beggining(black)" do
-        
-    end
-    xit "handles a board with pieces(white)" do
-
-    end 
-    xit "handles a board with pieces(black)" do
-
-    end
+                            [" ","p"," "," "," "," "," "," "],
+                            [""," "," "," "," "," "," "," "]]
+            c_cell = [4,3]
+            expect(bishop.possible_moves(white_player, c_cell, board)).to eql([[5,4],[6,5],[7,6],[5,2],[6,1],[3,2],[2,1]])
+        end
     end
 end
