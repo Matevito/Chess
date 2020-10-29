c723a0397b4c19ad973f42a67bbab251eb13708b
Mateo <madiazt@unal.edu.co>
2020-10-29 17:56:45 -0500

    added get_king method

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -12,25 +12,44 @@ class King < Board
     def possible_moves(player, position, board)
 
     end
-    def check?(player, position, board)
+    def check?(player, board)
         color = player.color
         current_board = board.board
-
+        king_position = get_king(color, current_board)
+        
+        # if the path of an enemy piece has the current cell of the king, is a check
         if color == "white"
             for y in range(0..7) do
                 for x in range(0..7)do
                     c_cell = current_board[y][x]
-                    return true if check_path(c_cell, current_board,"black").include?(position)
+                    return true if check_path(c_cell, current_board,"black").include?(king_position)
                 end
             end
         elsif color == "black"
             for y in range(0..7) do
                 for x in range(0..7)do
                     c_cell = current_board[y][x]
-                    return true if check_path(c_cell, current_board, "white").include?(position)
+                    return true if check_path(c_cell, current_board, "white").include?(king_position)
                 end
             end
         end
         return false
+    end
+    def get_king(color)
+        if color == "white"
+            for y in range(0..7) do
+                for x in range(0..7)do
+                    c_cell = board[y][x]
+                    return [y,x] if c_cell == "K"
+                end
+            end
+        elsif color == "black"
+            for y in range(0..7) do
+                for x in range(0..7)do
+                    c_cell = board[y][x]
+                    return [y,x] if c_cell == "k"
+                end
+            end
+        end
     end
 end
