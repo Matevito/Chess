7536af4d40245af50b87321b34c678cde8ae87fc
Mateo <madiazt@unal.edu.co>
2020-11-01 16:45:56 -0500

    added put_unicode method

--- lib/board.rb
+++ lib/board.rb
@@ -16,9 +16,50 @@ class Board
         return true
     end
     def print_board
-
+        board_to_print = @board.dup.map(&:dup)
+        board_to_print = put_unicode(board_to_print)
+        
     end
     def put_unicode(c_board)
-
+        uni_board = []
+        c_board.each do |cells|
+            row = []
+            cells.each do |c_cell|
+                case c_cell
+                when " "
+                    new_content = " "
+                when "t"
+                    new_content = "\u2656"
+                when "T"
+                    new_content = "\u265C"
+                when "n"
+                    new_content = "\u2658"
+                when "N"
+                    new_content = "\u265E"
+                when "b"
+                    new_content = "\u2657"
+                when "B"
+                    new_content = "\u265D"
+                when "q"
+                    new_content = "\u2655"
+                when "Q"
+                    new_content = "\u265B"
+                when "k"
+                    new_content = "\u2654"
+                when "K"
+                    new_content = "\u265A"
+                when "p"
+                    new_content = "\u2659"
+                when "P"
+                    new_content = "\u265F"
+                end
+                row << new_content
+            end
+            uni_board << row
+        end
+        return uni_board
     end
 end
+
+board = Board.new
+board.print_board
